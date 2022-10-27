b7b98435f4e0b9cd6e2ff5b460590b28e57ca230
Vojin <dvojinovic38@gmail.com>
2022-10-27 23:58:11 +0200

    Create project

---
+++ Todos/Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Dusan Vojinovic on 27.10.22..
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
